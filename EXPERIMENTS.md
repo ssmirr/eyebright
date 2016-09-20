4c0e5289740570a5a300dee7a61f73e3e496450d
Jason Ronallo <jronallo@gmail.com>
2016-09-20 19:56:35 -0400

    if a full size still already exists for this frame use that instead of extracting again

--- EXPERIMENTS.md
+++ EXPERIMENTS.md
@@ -10,7 +10,7 @@ You can see the sample videos here:
 <https://github.com/NCSU-Libraries/eyebright/tree/master/test/videos>
 No attempt is made yet to serve the actual video file but just still images from the video.
 
-If you have the development server running you can visit:
+If you have the development server running you can get a still image 8 seconds into the video by visiting:
 <http://localhost:8090/iiifv/water/8/full/600,/0/default.jpg>
 
 Otherwise use the included Apache server:
@@ -28,4 +28,9 @@ Included CC0 videos from [Pexels](https://videos.pexels.com/video-license):
 
 ### Video Stills: TODOs
 - consider using [yadif filter](http://ffmpeg.org/ffmpeg-all.html#yadif) for deinterlacing and [scale filter](http://ffmpeg.org/ffmpeg-all.html#scale) for resizing, especially if getting the full image region: `-filter:v 'yadif,scale=420:270'`
+- Allow for other ways to represent time? 00:00:08? even 00:00:08.435?
+- Allow for a duration of time? This would allow the sever to select an appropriate image to show. For instance the server could be smart and select the most representative still for the duration or pick on it already has or just create one from the middle.
+- Allow for getting back a particular frame? Instead of seconds we might have something like "frame:100"
+
+### Video Stills: Updates
 - If a full sized image is already available on the file system use that instead of extracting a still from the video using ffmpeg again.
--- app/models/video_image_extractor.rb
+++ app/models/video_image_extractor.rb
@@ -9,7 +9,7 @@ class VideoImageExtractor
     get_informer
     enrich_iiif_params
     # FIXME: pick better temporary images
-    @temp_out_image = Tempfile.new [@params[:id], '.tif']
+    @temp_out_image = Tempfile.new [@params[:id], '.jpg']
     @temp_response_image = Tempfile.new [@params[:id], ".#{@params[:format]}"]
   end
 
@@ -25,14 +25,36 @@ class VideoImageExtractor
   end
 
   def extract
-    # TODO: Handle time better. For now just convert to an integer to make sure it is safe
-    `ffmpeg -y -i #{@path} -ss #{@params[:time].to_i} -vframes 1 #{@temp_out_image.path}`
+    # Check for full size image first
+    if !full_size_image?
+      # TODO: Handle time better. For now just convert to an integer to make sure it is safe
+      `ffmpeg -y -i #{@path} -ss #{@params[:time].to_i} -vframes 1 #{@temp_out_image.path}`
+      FileUtils.mkdir_p full_size_image_directory
+      FileUtils.cp @temp_out_image.path, full_size_image_path
+    end
     `#{convert_cmd}`
     @temp_response_image
   end
 
+  def full_size_image?
+    File.exist? full_size_image_path
+  end
+
+  def full_size_image_path
+    File.join full_size_image_directory, "default.jpg"
+  end
+
+  def full_size_image_directory
+    File.join Rails.root,
+     "public/iiifv/#{@params[:id]}",
+     "#{@params[:time]}/full/full/0"
+  end
+
   def base_convert_cmd
-    cmd = "convert #{@temp_out_image.path} "
+    cmd = "convert "
+    # There should always be this full size image now
+    cmd << " #{full_size_image_path}"
+
     unless @iiif[:region] == 'full'
       cmd << " -crop #{@iiif[:region][:w]}x#{@iiif[:region][:h]}+#{@iiif[:region][:x]}+#{@iiif[:region][:y]}"
     end
