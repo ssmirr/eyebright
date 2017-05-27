f5c41d6738bad8ca75cd547d021f3b2bd03a8bfd
Jason Ronallo <jronallo@gmail.com>
2017-05-27 08:55:01 -0400

    fix OSD viewer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( video-viewer.js videojs-dash.min.js video-viewer.css )
+Rails.application.config.assets.precompile += %w( osd.js osd.css
+video-viewer.js videojs-dash.min.js video-viewer.css )
